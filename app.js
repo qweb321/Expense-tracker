dbdfd258f8bc0658f8ffc55f25e94f30bca0f247
Isis Lin <qweb32114@gmail.com>
2022-12-30 17:05:36 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 usePassport(app);
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user; //from passport deserializeUser
+  next();
+});
+
 app.use(routes);
 
 app.listen(port, () => {
