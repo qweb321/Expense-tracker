1dfc4be7ca555d0357c55b624ac4588b7962fe02
Isis Lin <qweb32114@gmail.com>
2022-12-31 19:50:38 +0800

    style: add category icon via fontawsome

--- model/seeder/categotySeeder.js
+++ model/seeder/categotySeeder.js
@@ -2,11 +2,11 @@ const Category = require("../category");
 const db = require("../../config/mongoose");
 
 const CATEGORY = {
-  家居物業: "https://fontawesome.com/icons/home?style=solid",
-  交通出行: "https://fontawesome.com/icons/shuttle-van?style=solid",
-  休閒娛樂: "https://fontawesome.com/icons/grin-beam?style=solid",
-  餐飲食品: "https://fontawesome.com/icons/utensils?style=solid",
-  其他: "https://fontawesome.com/icons/pen?style=solid",
+  家居物業: "fa-solid fa-house",
+  交通出行: "fa-solid fa-van-shuttle",
+  休閒娛樂: "fa-solid fa-face-grin-beam",
+  餐飲食品: "fa-solid fa-utensils",
+  其他: "fa-solid fa-pen",
 };
 
 db.once("open", (req, res) => {
